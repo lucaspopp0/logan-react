d695587f41913b7eda3b56728106f79dae57f675
Lucas Popp <lmp122@case.edu>
2020-08-26 15:32:01 -0400

    Add additional style rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,21 @@ module.exports = {
     },
     rules: {
         'prettier/prettier': 'error',
+        'no-await-in-loop': 'warn',
+        'max-params': ['error', 6],
+        camelcase: [
+            'warn',
+            {
+                properties: 'never',
+                allow: ['__test_only__'],
+            },
+        ],
+        'no-underscore-dangle': [
+            'error',
+            {
+                allowAfterThis: true,
+                allow: ['__test_only__', '__meta__'],
+            },
+        ],
     },
 };
